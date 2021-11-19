16a2c2f72be03d736973389346dc66d54c8e0f27
y.sokyrko <y.sokyrko@bankvostok.com.ua>
2021-11-19 14:11:17 +0200

    review 1.0

--- app/src/main/java/com/hfad/doctorapp/MainActivity.java
+++ app/src/main/java/com/hfad/doctorapp/MainActivity.java
@@ -1,44 +1,39 @@
 package com.hfad.doctorapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatTextView;
-import androidx.constraintlayout.helper.widget.Flow;
+import static android.widget.Toast.LENGTH_SHORT;
 
-import android.media.Image;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
+
 import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+    //todo should be private
     ImageView userLogo;
     AppCompatTextView viewAll;
     TextInputLayout endIcon;
+    private int duration = LENGTH_SHORT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //todo redundant cast
         userLogo = (ImageView) findViewById(R.id.user_account_logo);
-        userLogo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CharSequence popUpText = "Logo";
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
-                toast.show();
-
-            }
-        });
+        //todo it is better to put it in separate methods
+        setOnUserLogoClick();
         viewAll = findViewById(R.id.view_all);
         viewAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 CharSequence popUpText = "View all";
-                int duration = Toast.LENGTH_SHORT;
+                int duration = LENGTH_SHORT;
                 Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
                 toast.show();
             }
@@ -48,73 +43,86 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 CharSequence popUpText = "Search info";
-                int duration = Toast.LENGTH_SHORT;
+                int duration = LENGTH_SHORT;
                 Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
                 toast.show();
             }
         });
     }
 
+    private void setOnUserLogoClick() {
+        userLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //todo it is better to use String
+                CharSequence popUpText = "Logo";
+                //todo should be a common variable
+                int duration = LENGTH_SHORT;
+                Toast.makeText(MainActivity.this, popUpText, LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
     public void buttonClicked(View view) {
         if (view.getId() == R.id.button_consultation) {
             CharSequence popUpText = "Consultation";
-            int duration = Toast.LENGTH_SHORT;
+            int duration = LENGTH_SHORT;
             Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
             toast.show();
         } else if (view.getId() == R.id.button_dental) {
             CharSequence popUpText = "Dental";
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
+            int duration = LENGTH_SHORT;
+            Toast toast = Toast.makeText(MainActivity.this, popUpText, LENGTH_SHORT);
             toast.show();
         } else if (view.getId() == R.id.button_heart) {
             CharSequence popUpText = "Heart";
-            int duration = Toast.LENGTH_SHORT;
+            int duration = LENGTH_SHORT;
             Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
             toast.show();
 
         } else if (view.getId() == R.id.button_hospitals) {
             CharSequence popUpText = "Hospitals";
-            int duration = Toast.LENGTH_SHORT;
+            int duration = LENGTH_SHORT;
             Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
             toast.show();
 
         } else if (view.getId() == R.id.button_medicines) {
             CharSequence popUpText = "Medicines";
-            int duration = Toast.LENGTH_SHORT;
+            int duration = LENGTH_SHORT;
             Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
             toast.show();
-        }
-        else if (view.getId() == R.id.button_physician) {
+        } else if (view.getId() == R.id.button_physician) {
             CharSequence popUpText = "Physician";
-            int duration = Toast.LENGTH_SHORT;
+            int duration = LENGTH_SHORT;
             Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
             toast.show();
-        }
-        else if (view.getId() == R.id.button_skin) {
+        } else if (view.getId() == R.id.button_skin) {
             CharSequence popUpText = "Skin";
-            int duration = Toast.LENGTH_SHORT;
+            int duration = LENGTH_SHORT;
             Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
             toast.show();
-        }
-        else if (view.getId() == R.id.button_surgeon) {
+        } else if (view.getId() == R.id.button_surgeon) {
             CharSequence popUpText = "Surgeon";
-            int duration = Toast.LENGTH_SHORT;
+            int duration = LENGTH_SHORT;
             Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
             toast.show();
         }
     }
 
+    //todo incomprehensible naming
     public void menuSendwichButtonClicked(View view) {
         CharSequence popUpText = "Additional menu options";
-        int duration = Toast.LENGTH_SHORT;
+        int duration = LENGTH_SHORT;
         Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
         toast.show();
 
     }
 
+    //todo incomprehensible naming
     public void openButtonClicked(View view) {
         CharSequence popUpText = "Open description of this doctor";
-        int duration = Toast.LENGTH_SHORT;
+        int duration = LENGTH_SHORT;
         Toast toast = Toast.makeText(MainActivity.this, popUpText, duration);
         toast.show();
     }
